658f8341abca3c309aecf2d4069b3e729fb268b9
iHarshSinha <harsh.sinha@iiitb.ac.in>
2025-09-30 16:31:48 +0530

    first commit

---
+++ books.js
@@ -0,0 +1,40 @@
+// Sample in-memory Book Catalog data
+let books = [
+  {
+    id: 1,
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    year: 2008,
+    genre: "Programming"
+  },
+  {
+    id: 2,
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt, David Thomas",
+    year: 1999,
+    genre: "Programming"
+  },
+  {
+    id: 3,
+    title: "Atomic Habits",
+    author: "James Clear",
+    year: 2018,
+    genre: "Self-help"
+  },
+  {
+    id: 4,
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960,
+    genre: "Fiction"
+  },
+  {
+    id: 5,
+    title: "1984",
+    author: "George Orwell",
+    year: 1949,
+    genre: "Dystopian"
+  }
+];
+
+module.exports = books;
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+let books = require("./books");
+
+
+const app = express();
+const PORT = 3000;
+
+
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to the Book Catalog API!");
+});
+
+app.get("/getAll",(req,res)=>{
+    return res.json(books);
+})
+
+app.post("/createBook",(req,res)=>{
+    books.push(req.body);
+    
+    return res.status(201).json(books);
+})
+
+app.put("/updateBook/:id",(req,res)=>{
+    const {id} = req.params;
+    const {title,author,year,genre} = req.body;
+    
+    let book = books.find((b)=>b.id === Number(id));
+    if(book){
+        book.title = title;
+        book.author = author;
+        book.year = year;
+        book.genre = genre;
+        return res.json(book);
+    }
+    else{
+        return res.status(404).json({message:"Book not found"});
+    }
+
+})
+// Delete a book by id
+app.delete("/deleteBook/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = books.findIndex((b) => b.id === Number(id));
+  if (index !== -1) {
+    const deletedBook = books.splice(index, 1); // remove the book from array
+    res.json({ message: "Book deleted successfully!", book: deletedBook[0] });
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
